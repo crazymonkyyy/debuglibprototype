e58b89beff606eef431dd47ca086e986a11de9ff
crazymonkyyy <crazymonkyyy@gmail.com>
2024-12-02 08:47:45 -0700

    idk putting on hold for a while, havnt been productive for a while

--- mkydbg.d
+++ mkydbg.d
@@ -5,7 +5,7 @@ alias currentfile()=innate!(string,"","current-logging");
 
 import softtab;
 import unmix;
-
+// internal detail: controls filtering by file and file sections
 bool fileheaderconsider()(string s){
 	//import std;
 	//writeln(s,activefile!(),s!=activefile!());
@@ -18,22 +18,23 @@ bool fileheaderconsider()(string s){
 	}
 	return true;
 }
-unittest{
-	fileheaderconsider("AHHH");
-	fileheaderconsider("AHHH");
-	fileheaderconsider("AHHH");
-	fileheaderconsider("foo");
-	activefile!()="bar";
-	fileheaderconsider("foobar");
-	fileheaderconsider("bar");
-}
+//unittest{
+//	fileheaderconsider("AHHH");
+//	fileheaderconsider("AHHH");
+//	fileheaderconsider("AHHH");
+//	fileheaderconsider("foo");
+//	activefile!()="bar";
+//	fileheaderconsider("foobar");
+//	fileheaderconsider("bar");
+//}
 void unmixln(int loglvl_=0,string file=__FILE__,int line=__LINE__,T)(T t){
 	if(fileheaderconsider(file)){
 		softln(line,loglvl_,unmix_!(file,line).simplify,"\t//",t);
 }}
-int foo(string)=>1;
+// int foo(string)=>1;
 //unittest{
 //	printunderline("foo");
 //	"hello".foo.unmixln;
 //	printunderline("bar");
 //}
+import parsecli;
